a7674513c93b268533742af7f6c9ff25a11c7c9a
KeishaRuiz <98569242+KeishaRuiz@users.noreply.github.com>
2022-02-03 18:29:58 -0600

    Add files via upload

---
+++ MyVarProject/MyVarProject/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+
+
+namespace VariablesProj
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+
+            int myVal = 7;
+
+
+
+
+            myVal = -1 + 4 * myVal;
+
+            Console.WriteLine(myVal);
+
+            myVal = (35 + 5) % myVal;
+
+            Console.WriteLine(myVal);
+
+            myVal = 14 + -4 * 6 / 12;
+
+            Console.WriteLine(myVal);
+
+            myVal /= 2 + 12 / 6 * 1 - myVal % 2;
+
+            Console.WriteLine(myVal);
+
+
+
+            {
+                int myVal2 = 0;
+
+                myVal2 *= myVal * myVal;
+
+
+            
+            if (myVal2 > 10)
+                {
+                    Console.WriteLine("My Var is greater than 10");
+                }
+                else
+                {
+                    Console.WriteLine("My Var is less than 10");
+                }
+            }
+
+
+
+
+        }
+    }
+}
